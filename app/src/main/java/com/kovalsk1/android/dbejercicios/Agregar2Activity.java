6a0a22c25bff51a6e1e61b870a7beee33247f693
kowa1ski <kowalski@gmx.es>
2017-08-20 17:11:07 +0200

    layout de agregación hecho. Creado el método agregar. Comprobado y funcionando.

--- app/src/main/java/com/kovalsk1/android/dbejercicios/Agregar2Activity.java
+++ app/src/main/java/com/kovalsk1/android/dbejercicios/Agregar2Activity.java
@@ -6,8 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Agregar2Activity extends AppCompatActivity {
+
+    // Declaramos las variables para recoger la información de los editText
+    private TextView nombre, apellidos, edad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,51 @@ public class Agregar2Activity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        // relacionamos las variables con los editText
+        nombre = (TextView) findViewById(R.id.editTextNombre);
+        apellidos = (TextView) findViewById(R.id.editTextApellido);
+        edad = (TextView) findViewById(R.id.editTextEdad);
+
+    }
+
+    // Creamos este método , agregar, y el , ComprobarCampos , y
+    // lo comprobamos que funciona lanzando los toast
+    public void agregar(View view){
+
+        if (ComprobarCampos()) {
+            Toast.makeText(this, "no hay campos vacíos", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "existen campos que están vacíos", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean ComprobarCampos () {
+        if (nombre.getText().toString().isEmpty() || apellidos.getText().toString().isEmpty()
+                || edad.getText().toString().isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
