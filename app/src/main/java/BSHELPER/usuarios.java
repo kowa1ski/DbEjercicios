b3ddaa3948e9a86600ab1995372205e9068b61db
kowa1ski <kowalski@gmx.es>
2017-08-20 11:47:18 +0200

    Construimos la clase usuarios

--- app/src/main/java/BSHELPER/usuarios.java
+++ app/src/main/java/BSHELPER/usuarios.java
@@ -5,4 +5,53 @@ package BSHELPER;
  */
 
 public class usuarios {
+
+
+    // creamos las variables de las que consta la clase.
+    private int usuario, edad;
+    private String nombre, apellidos;
+
+    // generamos el constructor automáticamente con clic derecho y generate constructor.
+    public usuarios(int usuario, int edad, String nombre, String apellidos) {
+        this.usuario = usuario;
+        this.edad = edad;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    // generamos los getters y setters de manera automática
+    // con generate.
+
+
+    public int getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(int usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
 }
